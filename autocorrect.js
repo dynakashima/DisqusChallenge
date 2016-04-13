f05e7f9f98da7cd0a4b61c28b6d528833efea77c
dynakashima <nakashima.dy@gmail.com>
2016-04-13 10:30:15 -0700

    finished function to generate possiblities for repeating letters

--- autocorrect.js
+++ autocorrect.js
@@ -2,9 +2,9 @@
 # Word Correction Take-Home
 
 Write a program in JavaScript that reads a large list of English words 
-(e.g. from /usr/share/dict/words on a unix system) into memory, and then 
-reads words from stdin, and prints either the best spelling correction, 
-or "NO CORRECTION" if no suitable correction can be found.
+(e.g. from /usr/share/dict/words on a unix system) into memory, and 
+then reads words from stdin, and prints either the best spelling 
+correction, or "NO CORRECTION" if no suitable correction can be found.
 
 The program should print "> " as a prompt before reading each word, 
 and should loop until killed.
@@ -30,21 +30,22 @@ conspiracy
 consperrICY
 
 
-Your solution should be faster than O(n) per word checked, where n is the
-length of the dictionary. That is to say, you can't scan the dictionary 
-every time you want to spellcheck a word.
+Your solution should be faster than O(n) per word checked, where n is 
+the length of the dictionary. That is to say, you can't scan the 
+dictionary every time you want to spellcheck a word.
 
-If there are many possible corrections of an input word, your program can
-choose one in any way you like, however your results *must* match the 
-examples above (e.g. "sheeeeep" should return "sheep" and not "shap").
+If there are many possible corrections of an input word, your program 
+can choose one in any way you like, however your results *must* match 
+the examples above (e.g. "sheeeeep" should return "sheep" and not 
+"shap").
 
 
 ## Incorrect Word Generator
 
-Write a second program that generates words with spelling mistakes of the
-above form, starting with correctly spelled English words. Pipe its output
-into the first program and verify that there are no occurrences of 
-"NO CORRECTION" in the output.
+Write a second program that generates words with spelling mistakes of 
+the above form, starting with correctly spelled English words. Pipe its
+output into the first program and verify that there are no occurrences 
+of "NO CORRECTION" in the output.
 */
 
 var fs = require("fs");
@@ -56,14 +57,21 @@ var Deque = require("collections/deque");
 
 // var data = fs.readFileSync('dictionary.txt');
 // var dictionary = data.toString().trim().split('\n');
+
+// mock dictionary for development
 var dictionary = new Set([
 	'inside',
 	'job',
 	'sheep',
 	'conspiracy'
 ]);
+function Pair(letter, freq) {
+    this.letter = letter;
+    this.frequency = freq
+}
 
 var VOWELS = new Set(['a', 'e', 'i', 'o', 'u']);
+// console.log(VOWELS.difference(['a']).toArray())
 
 var autocorrect = function(word) {
 	// handle lowercase
@@ -83,16 +91,66 @@ function genVowelWords(word) {
 
 }
 
-var genRepeatLetters = function(word) {
+// algorithm goes like this... 
+// create a set if the frequency of letter is one add
+// letter to end of every element of the array if it is
+function genRepeatLetters(frequencies) {
+	var toRet = new Set(['']);
 
+	frequencies.forEach(function(pair) {
+		var letter = pair.letter;
+		var freq = pair.frequency;
+		toRet = addLettersToSet(letter, freq, toRet);
+	});
+	return toRet;
 };
 
-function Pair(letter, freq) {
-    this.letter = letter;
-    this.freq = freq
+// var a = new Pair('a', 3);
+// var b = new Pair('b', 2);
+// console.log(genRepeatLetters([a,b]).toArray());
+
+// helper function to that given a letter and a frequency:f
+// will make f copies of the current set and return a new
+// and for each copy, will append the letter f times
+// should return 'a', 'aa', 'aaa'
+function addLettersToSet(letter, frequency, currSet) {
+	var letToAppend = '';
+	
+	var retSet = new Set();
+	
+	for (var j = 0; j < frequency; j++) {
+		var tempSet = currSet.clone();
+		letToAppend += letter;
+		var setToAdd = addLettersToSet(letToAppend, tempSet);
+		retSet.addEach(setToAdd);
+	};
+	return retSet;
 }
+// console.log(addLettersToSet('a', 3, new Set([''])).toArray());
+// console.log(addLettersToSet('b', 2, new Set(['a', 'aa', 'aaa'])).toArray());
+
+
+
+// returns a new set with letters added doesNot modify inputSet
+function addLettersToSet(letters, set) {
+	var setToRet = new Set();
+	set.forEach(function(key) {
+		setToRet.add(key + letters);
+	});
+	return setToRet;
+}
+// given a frequency:f will 
+// create f copies of the set
+// then will a
+
+// var testSet = new Set(['a','aa', 'aaa']);
+
+// console.log(testSet.toArray())
+// console.log(addLettersToSet('e', testSet).toArray());
 
 // build array with count of elements and letter as key
+// this is just for ease to elinate checks within the 
+// program for if we are in a duplicate
 var frequencyArray = function(word) {
 	var prevLetter = word[0];
 	var toBuild = [new Pair(prevLetter, 1)];
@@ -101,7 +159,7 @@ var frequencyArray = function(word) {
 		var currLetter = word[i];
 		if (currLetter === prevLetter) {
 			var pair = toBuild.peekBack();
-			pair.freq += 1;
+			pair.frequency += 1;
 		}
 		else {
 			toBuild.push(new Pair(currLetter, 1));
@@ -110,11 +168,15 @@ var frequencyArray = function(word) {
 	};
 	return toBuild;
 };
-
+// console.log(frequencyArray('helloo'));
+// console.log(genRepeatLetters(frequencyArray('helloo')).toArray());
 // test cases
 function assert(actual, expected) {
 	if (expected !== actual) { 
-		console.error('FAILED -> got:', actual + ',', 'expected:', expected);
+		console.error(
+			'FAILED -> got:', actual + ',',
+			'expected:', expected
+		);
 		return 1;
 	}
 	console.log('passed')
